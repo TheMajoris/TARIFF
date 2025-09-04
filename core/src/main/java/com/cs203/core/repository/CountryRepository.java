5f0da8d22d1a452410ffa36ddca97f740b2ca19a
TheMajoris <74721713+TheMajoris@users.noreply.github.com>
2025-09-04 22:39:03 +0800

    Changed a line that was copy pasted from UserRepository (whoops)

--- core/src/main/java/com/cs203/core/repository/CountryRepository.java
+++ core/src/main/java/com/cs203/core/repository/CountryRepository.java
@@ -1,9 +1,9 @@
 package com.cs203.core.repository;
 
-import com.cs203.core.entity.UserEntity;
+import com.cs203.core.entity.CountryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CountryRepository extends JpaRepository<UserEntity, Long> {
+public interface CountryRepository extends JpaRepository<CountryEntity, Long> {
 }
