140a7a0a1323235bad4d3f512f70f7acefd3a5b3
TheMajoris <74721713+TheMajoris@users.noreply.github.com>
2025-09-04 22:38:20 +0800

    Adding country table along with it's repository file

---
+++ core/src/main/java/com/cs203/core/repository/CountryRepository.java
@@ -0,0 +1,9 @@
+package com.cs203.core.repository;
+
+import com.cs203.core.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CountryRepository extends JpaRepository<UserEntity, Long> {
+}
