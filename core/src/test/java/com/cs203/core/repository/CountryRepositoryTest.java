7358f669d14ec7798d28243d28644899cf7e0718
TheMajoris <74721713+TheMajoris@users.noreply.github.com>
2025-09-05 23:52:15 +0800

    Implementation for test of CountryRepositoryTest

--- core/src/test/java/com/cs203/core/repository/CountryRepositoryTest.java
+++ core/src/test/java/com/cs203/core/repository/CountryRepositoryTest.java
@@ -1,7 +1,6 @@
 package com.cs203.core.repository;
 
 import com.cs203.core.entity.*;
-import com.cs203.core.repository.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
