5ed593a738ba763212a9a36054b434722f8bea22
yijiyap <yijiyap@gmail.com>
2025-09-09 15:51:25 +0800

    chore: add token service impl

--- core/src/main/java/com/cs203/core/controller/AuthController.java
+++ core/src/main/java/com/cs203/core/controller/AuthController.java
@@ -3,6 +3,8 @@ package com.cs203.core.controller;
 import com.cs203.core.service.AuthService;
 import com.cs203.core.service.TokenService;
 import com.cs203.core.utils.CookieUtil;
+import org.antlr.v4.runtime.Token;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +14,13 @@ public class AuthController {
     private final AuthService authService;
     private final CookieUtil cookieUtil;
     private final TokenService tokenService;
+
+    @Autowired
+    public AuthController(AuthService authService,
+                          CookieUtil cookieUtil,
+                          TokenService tokenService) {
+        this.authService = authService;
+        this.cookieUtil = cookieUtil;
+        this.tokenService = tokenService;
+    }
 }
--- core/src/main/java/com/cs203/core/service/impl/TokenServiceImpl.java
+++ core/src/main/java/com/cs203/core/service/impl/TokenServiceImpl.java
@@ -2,17 +2,22 @@ package com.cs203.core.service.impl;
 
 import com.cs203.core.dto.responses.GenericResponseDTO;
 import com.cs203.core.entity.RefreshToken;
+import com.cs203.core.entity.UserEntity;
 import com.cs203.core.repository.TokenRepository;
 import com.cs203.core.repository.UserRepository;
 import com.cs203.core.service.TokenService;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+import java.time.Duration;
 import java.time.OffsetDateTime;
 import java.time.ZonedDateTime;
 import java.util.UUID;
 
+@Service
 public class TokenServiceImpl implements TokenService {
     @Value("${jwt.refresh.duration}")
     private long refreshTokenValidity;
@@ -30,8 +35,18 @@ public class TokenServiceImpl implements TokenService {
     @Override
     @Transactional
     public String createRefreshToken(String email) {
+        UserEntity user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException(email));
 
-        return "TODO";
+        final UUID tokenId = UUID.randomUUID();
+        final RefreshToken refreshToken = new RefreshToken();
+
+        refreshToken.setToken(tokenId);
+        refreshToken.setUser(user);
+        refreshToken.setExpiresAt(OffsetDateTime.now().plus(Duration.ofMinutes(refreshTokenValidity)));
+
+        tokenRepository.save(refreshToken);
+        return refreshToken.getToken().toString();
     }
 
     @Override
