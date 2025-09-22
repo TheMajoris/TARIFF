ddf215026110c35c7305a443fa28c49e8feebb10
Liu JiaJun <jiajunliupersonal@gmail.com>
2025-09-22 17:31:28 +0800

    Update UserEntity.java

--- core/src/main/java/com/cs203/core/entity/UserEntity.java
+++ core/src/main/java/com/cs203/core/entity/UserEntity.java
@@ -5,7 +5,15 @@ import java.time.LocalDateTime;
 import org.hibernate.annotations.CreationTimestamp;
 
 import com.cs203.core.enums.UserRole;
-import jakarta.persistence.*;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
