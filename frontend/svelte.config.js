95304b690c44ac870b088e62184ef117bdd00848
TheMajoris <74721713+TheMajoris@users.noreply.github.com>
2025-10-16 12:18:30 +0800

    Added some stuff

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,12 +1,23 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+    // Consult [https://svelte.dev/docs/kit/integrations](https://svelte.dev/docs/kit/integrations)
+    // for more information about preprocessors
+    preprocess: vitePreprocess(),
+
+    kit: {
+        // Use the static adapter to output a folder of static files
+        adapter: adapter({
+            // This is the directory where the build output will be placed.
+            // It must match the directory your Dockerfile copies from.
+            pages: 'build',
+            assets: 'build',
+            fallback: null,
+            precompress: false
+        })
+    }
 };
 
 export default config;
