afa6a4e673e39a69c1d9dc3f20a0b6005615f182
Kazor Vinci <kazorvinci@gmail.com>
2025-10-20 20:36:28 +0800

    Basic CRUD implemented for ProductCategories
    - Get by pagination and get by id
    - Post with duplicate hs code check
    - Update ProductCategories will cascade
    - Delete ProductCategories will cascade

---
+++ core/src/main/java/com/cs203/core/service/ProductCategoriesService.java
@@ -0,0 +1,22 @@
+package com.cs203.core.service;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.cs203.core.dto.CreateProductCategoriesDto;
+import com.cs203.core.dto.GenericResponse;
+import com.cs203.core.dto.ProductCategoriesDto;
+
+public interface ProductCategoriesService {
+    public Page<ProductCategoriesDto> getProductCategories(Pageable pageable);
+
+    public GenericResponse<ProductCategoriesDto> getProductCategoriesById(Long id);
+
+    public GenericResponse<ProductCategoriesDto> createProductCategories(CreateProductCategoriesDto dto);
+
+    public GenericResponse<ProductCategoriesDto> updateProductCategories(Long id, ProductCategoriesDto dto);
+
+    public GenericResponse<Void> deleteProductCategories(Long id);
+
+    public GenericResponse<Void> forceDeleteProductCategories(Long id);
+}
