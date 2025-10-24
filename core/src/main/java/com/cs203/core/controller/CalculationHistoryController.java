4081d283dc4345f0a4af3a327419b32e4781f28d
Liu JiaJun <jiajunliupersonal@gmail.com>
2025-10-24 17:24:26 +0800

    Get user from jwt to saveCalculation

--- core/src/main/java/com/cs203/core/controller/CalculationHistoryController.java
+++ core/src/main/java/com/cs203/core/controller/CalculationHistoryController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,7 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.cs203.core.dto.GenericResponse;
 import com.cs203.core.dto.SavedCalculationDto;
 import com.cs203.core.dto.requests.SaveCalculationRequestDTO;
-import com.cs203.core.entity.UserEntity;
 import com.cs203.core.service.CalculationHistoryService;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -32,16 +32,16 @@ public class CalculationHistoryController {
     @PostMapping
     public ResponseEntity<GenericResponse<SavedCalculationDto>> saveCalculation(
         @Valid @RequestBody SaveCalculationRequestDTO requestDto, Authentication authentication) {
-            UserEntity user = (UserEntity) authentication.getPrincipal();
-            Long userId = user.getId();
+            Jwt jwt = (Jwt) authentication.getPrincipal();
+            Long userId = Long.parseLong(jwt.getSubject());
             GenericResponse<SavedCalculationDto> response = calculationHistoryService.saveCalculation(requestDto,userId);
         return new ResponseEntity<>(response, response.getStatus());
     }
 
     @GetMapping
     public ResponseEntity<GenericResponse<List<SavedCalculationDto>>> getCalculationsByUserId(Authentication authentication) {
-            UserEntity user = (UserEntity) authentication.getPrincipal();
-            Long userId = user.getId();
+            Jwt jwt = (Jwt) authentication.getPrincipal();
+            Long userId = Long.parseLong(jwt.getSubject());
             GenericResponse<List<SavedCalculationDto>> response = calculationHistoryService.getCalculationsByUserId(userId);
         return new ResponseEntity<>(response, response.getStatus());
     }    
