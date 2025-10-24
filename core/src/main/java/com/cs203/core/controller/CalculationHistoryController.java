b6da84e0b64f1d7a92e0db079efd88268ffd9fd9
Liu JiaJun <jiajunliupersonal@gmail.com>
2025-10-24 17:05:55 +0800

    Allow authenticated user to save calculation

--- core/src/main/java/com/cs203/core/controller/CalculationHistoryController.java
+++ core/src/main/java/com/cs203/core/controller/CalculationHistoryController.java
@@ -3,9 +3,9 @@ package com.cs203.core.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +19,6 @@ import com.cs203.core.service.CalculationHistoryService;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Tag(name = "Calculation History")
@@ -31,7 +30,6 @@ public class CalculationHistoryController {
     CalculationHistoryService calculationHistoryService;
 
     @PostMapping
-    @PreAuthorize("hasRole('TRADER')")
     public ResponseEntity<GenericResponse<SavedCalculationDto>> saveCalculation(
         @Valid @RequestBody SaveCalculationRequestDTO requestDto, Authentication authentication) {
             UserEntity user = (UserEntity) authentication.getPrincipal();
@@ -41,7 +39,6 @@ public class CalculationHistoryController {
     }
 
     @GetMapping
-    @PreAuthorize("hasRole('TRADER')")
     public ResponseEntity<GenericResponse<List<SavedCalculationDto>>> getCalculationsByUserId(Authentication authentication) {
             UserEntity user = (UserEntity) authentication.getPrincipal();
             Long userId = user.getId();
