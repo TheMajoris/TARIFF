0bb0aabeaa912f4b901011f163654ac7f4c569c2
yquanjun <yquanjun@gmail.com>
2025-11-07 03:24:38 +0800

    separate the docker start for e2e tests

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-	globalSetup: './e2e/global-setup.js',
-	globalTeardown: './e2e/global-teardown.js',
+	// globalSetup: './e2e/global-setup.js',
+	// globalTeardown: './e2e/global-teardown.js',
 	workers: 1,
 	webServer: {
 		command: 'npm run build && npm run preview',
